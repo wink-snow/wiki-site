92eff1e0413c93c9dbce0cf4bfa3b880934da394
千山暮雪 <li23258@qq.com>
2025-06-10 09:27:24 +0800

    initialize(repo): Fisrt commit.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="navbar bg-base-100 shadow-sm">
+        <a class="title">Wiki</a>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/WikiTable.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="overflow-x-auto">
+        <table class="table table-xs table-pin-rows table-pin-cols">
+            <thead>
+            <tr>
+                <th></th>
+                <td>Name</td>
+                <td>Job</td>
+                <td>company</td>
+                <td>location</td>
+                <td>Last Login</td>
+                <td>Favorite Color</td>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Littel, Schaden and Vandervort</td>
+                <td>Canada</td>
+                <td>12/16/2020</td>
+                <td>Blue</td>
+                <th>1</th>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr>
+                <th></th>
+                <td>Name</td>
+                <td>Job</td>
+                <td>company</td>
+                <td>location</td>
+                <td>Last Login</td>
+                <td>Favorite Color</td>
+                <th></th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+tr {
+    min-height: max-content;
+    text-align: center;
+    vertical-align: middle;
+}
+</style>
